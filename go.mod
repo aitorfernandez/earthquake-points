6ef8b2aaa9727c735239121c792dfe50f780ca9e
Aitor Fernandez <helloimaitor@gmail.com>
2021-03-08 21:20:05 +0000

    add gorilla/mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aitorfernandez/earthquake-points
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var port int
+
+func init() {
+	flag.IntVar(&port, "port", 8010, "server port")
+}
+
+// Handler handles "/" requests.
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
-	fmt.Println("vim-go")
+	getAddr := func() string {
+		return fmt.Sprintf(":%d", port)
+	}
+
+	flag.Parse()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", Handler)
+	srv := &http.Server{
+		Addr:    getAddr(),
+		Handler: r,
+	}
+	log.Print("starting server on" + getAddr())
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("server.ListenAndServe %v", err)
+	}
 }
