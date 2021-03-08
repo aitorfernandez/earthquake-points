034889db0beb04970bbe0615d627eb668625b2bd
Aitor Fernandez <helloimaitor@gmail.com>
2021-03-08 20:47:06 +0000

    add openstreetmap/leaflet map

---
+++ public/app.js
@@ -0,0 +1,17 @@
+const map = L.map('map', {
+  center: [20.0, 5.0],
+  maxZoom: 4,
+  minZoom: 2,
+  zoomDelta: 0.5,
+  zoomSnap: 0,
+})
+
+const cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  attribution: cartodbAttribution
+}).addTo(map)
+
+L.tileLayer('http://localhost:8010/{z}/{x}/{y}.png', {}).addTo(map);
+
+map.setView([0, 0], 0)
